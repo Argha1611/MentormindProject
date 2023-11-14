6f2fd9200a8e2e80bb2c226d13b749c61437ef26
Arghadeep Chakraborty <142204265+Argha1611@users.noreply.github.com>
2023-11-14 14:10:55 +0530

    Update UserService.java

--- src/main/com/project/mentormind/service/UserService.java
+++ src/main/com/project/mentormind/service/UserService.java
@@ -1,1 +1,24 @@
-
+package com.project.mentormind.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.project.mentormind.entity.User;
+import com.project.mentormind.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}

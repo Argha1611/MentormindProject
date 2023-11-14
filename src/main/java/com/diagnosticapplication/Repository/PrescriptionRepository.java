77f950a9b31638a40e6930d088b1ba3e136803de
Arghadeep Chakraborty <142204265+Argha1611@users.noreply.github.com>
2023-11-14 14:54:42 +0530

    Delete src/main/java/com/diagnosticapplication/Repository/PrescriptionRepository.java

--- src/main/java/com/diagnosticapplication/Repository/PrescriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.diagnosticapplication.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.diagnosticapplication.Entity.Prescription;
-@Repository
-public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
-    // You can define custom query methods if needed
-}

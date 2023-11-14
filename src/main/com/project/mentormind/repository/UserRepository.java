66b5125b567173a8bc8615fc0e96be2d805d064e
Arghadeep Chakraborty <142204265+Argha1611@users.noreply.github.com>
2023-11-14 14:24:20 +0530

    Delete src/main/com/project/mentormind directory

--- src/main/com/project/mentormind/repository/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.project.mentormind.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.mentormind.entity.User;
-public interface UserRepository extends JpaRepository<User, Long> {
-}

4d48604661707c083dd9ea8865993692d9e17b5c
Arghadeep Chakraborty <142204265+Argha1611@users.noreply.github.com>
2023-11-14 14:26:28 +0530

    Create Test.java

---
+++ src/main/java/com/project/mentormind/entity/Test.java
@@ -0,0 +1,16 @@
+package com.project.mentormind.entity;
+import java.util.List;
+public class Test {
+	 private List<String> tests;
+	    public Test() {
+	    }
+	    public Test(List<String> tests) {
+	        this.tests = tests;
+	    }
+	    public List<String> getTests() {
+	        return tests;
+	    }
+	    public void setTests(List<String> tests) {
+	        this.tests = tests;
+	    }
+	}
